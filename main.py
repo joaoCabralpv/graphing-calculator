17653bb4eeb03f79c7419b2043a42ff643b3d960
unknown <joaocabralpv@gmail.com>
2025-10-16 16:38:33 +0000

    Unary -

--- main.py
+++ main.py
@@ -37,30 +37,34 @@ operators:dict[str,Operator] = {
     "+": Operator(lambda a: a[0]+a[1],2,1),
     "-": Operator(lambda a: a[0]-a[1],2,1),
     "*": Operator(lambda a: a[0]*a[1],2,2),
-    "/": Operator(lambda a: a[0]/a[1],2,2)
+    "/": Operator(lambda a: a[0]/a[1],2,2),
+    "u-":Operator(lambda a: -a[0],1,999)
 }
 
 
-output_stack:list[Symbol] =[Symbol.from_float(1),Symbol.from_float(2),Symbol.from_float(4),Symbol.from_operator(operators["*"]),Symbol.from_operator(operators["+"]),Symbol.from_float(3),Symbol.from_operator(operators["-"])]
-
-solve_stack:list = []
-
-for symbol in output_stack:
-    if symbol.type == SymbolType.NUMBER:
-        solve_stack.append(symbol.represented)
-        continue
-
-    if symbol.type == SymbolType.OPERATOR:
-        operator = symbol.represented
-        func = operator.func
-        n_arguments = operator.n_arguments
-        list_arguments = []
-
-        for i in range(n_arguments):
-            list_arguments.append(solve_stack.pop())
-
-        list_arguments.reverse()
-        solve_stack.append(func(list_arguments))
+rpn_stack:list[Symbol] =[Symbol.from_float(22),Symbol.from_operator(operators["u-"]),Symbol.from_float(3),Symbol.from_operator(operators["+"])]
 
 
-print(solve_stack[0])
+
+def compute(rpn_stack):
+    solve_stack:list = []
+
+    for symbol in rpn_stack:
+        if symbol.type == SymbolType.NUMBER:
+            solve_stack.append(symbol.represented)
+            continue
+
+        if symbol.type == SymbolType.OPERATOR:
+            operator = symbol.represented
+            func = operator.func
+            n_arguments = operator.n_arguments
+            list_arguments = []
+
+            for i in range(n_arguments):
+                list_arguments.append(solve_stack.pop())
+
+            list_arguments.reverse()
+            solve_stack.append(func(list_arguments))
+    return solve_stack[0]
+
+print(compute(rpn_stack))
